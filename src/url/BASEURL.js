02584792fe53ac2d8f4dfb3adad7cfba0d4c47fc
kasinathan28 <way2kasinathb35@gmail.com>
2024-04-28 16:06:38 +0530

    Base Urls

--- src/url/BASEURL.js
+++ src/url/BASEURL.js
@@ -1,3 +1,5 @@
-exports.BASEURL = "http://localhost:5000"
+// exports.BASEURL = "http://localhost:5000"
 exports.IMAGEURL = "http://localhost:5000/uploads"
 exports.SOCKET_URL = "http://localhost:5000"
+
+exports.BASEURL ="https://res-server-7eve.onrender.com"
